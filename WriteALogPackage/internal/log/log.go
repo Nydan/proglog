765e930d672a453087becf5eb61559fc19ae4fe1
Danny Aguswahyudi <madannyaguswahyudi@gmail.com>
2020-09-17 22:33:58 +0700

    NewLog function

---
+++ WriteALogPackage/internal/log/log.go
@@ -0,0 +1,67 @@
+package log
+
+import (
+	"io/ioutil"
+	"path"
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type Log struct {
+	mu sync.RWMutex
+
+	Dir    string
+	Config Config
+
+	activeSegment *segment
+	segments      []*segment
+}
+
+func NewLog(dir string, c Config) (*Log, error) {
+	if c.Segment.MaxStoreBytes == 0 {
+		c.Segment.MaxStoreBytes = 1024
+	}
+	if c.Segment.MaxIndexBytes == 0 {
+		c.Segment.MaxIndexBytes = 1024
+	}
+	l := &Log{
+		Dir:    dir,
+		Config: c,
+	}
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	var baseOffsets []uint64
+
+	for _, file := range files {
+		offStr := strings.TrimSuffix(
+			file.Name(),
+			path.Ext(file.Name()),
+		)
+		off, _ := strconv.ParseUint(offStr, 10, 0)
+		baseOffsets = append(baseOffsets, off)
+	}
+
+	sort.Slice(baseOffsets, func(i, j int) bool {
+		return baseOffsets[i] < baseOffsets[j]
+	})
+
+	for i := 0; i < len(baseOffsets); i++ {
+		if err = l.newSegment(baseOffsets[i]); err != nil {
+			return nil, err
+		}
+		// baseOffset contains dup for index and store so we skip the dup
+		i++
+	}
+
+	if l.segments == nil {
+		if err = l.newSegment(c.Segment.InitialOffset); err != nil {
+			return nil, err
+		}
+	}
+	return l, nil
+}
